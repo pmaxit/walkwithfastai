eddcb04e31e32ba9f88319e1f5f716ebfb8f67ed
Puneet Girdhar <puneetgirdhar.in@gmail.com>
2024-08-30 17:15:24 -0500

    adding first few lines

--- walkwithfastai/_modidx.py
+++ walkwithfastai/_modidx.py
@@ -5,4 +5,9 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://pmaxit.github.io',
                 'git_url': 'https://github.com/pmaxit/walkwithfastai',
                 'lib_path': 'walkwithfastai'},
-  'syms': {'walkwithfastai.core': {'walkwithfastai.core.foo': ('core.html#foo', 'walkwithfastai/core.py')}}}
+  'syms': { 'walkwithfastai.core': { 'walkwithfastai.core.Number': ('core.html#number', 'walkwithfastai/core.py'),
+                                     'walkwithfastai.core.Number.__add__': ('core.html#number.__add__', 'walkwithfastai/core.py'),
+                                     'walkwithfastai.core.Number.__init__': ('core.html#number.__init__', 'walkwithfastai/core.py'),
+                                     'walkwithfastai.core.Number.__repr__': ('core.html#number.__repr__', 'walkwithfastai/core.py'),
+                                     'walkwithfastai.core.foo': ('core.html#foo', 'walkwithfastai/core.py'),
+                                     'walkwithfastai.core.say_hello': ('core.html#say_hello', 'walkwithfastai/core.py')}}}
